df1b938ee9503c27065fe86f46d98df90efe42f5
josephrosh12@icloud.com <josephrosh12@icloud.com>
2023-02-16 17:15:21 +0200

    First commit on filter project

---
+++ filter.js
@@ -0,0 +1,59 @@
+const data = [
+  {
+    id: 1,
+    name: "Invicta Men's Pro Diver",
+    img: "./img/f1.jpg",
+    price: 74,
+    cat: "Dress",
+  },
+  {
+    id: 11,
+    name: "Invicta Men's Pro Diver 2",
+    img: "./img/f2.jpg",
+    price: 74,
+    cat: "Dress",
+  },
+  {
+    id: 2,
+    name: "Timex Men's Expedition Scout ",
+    img: "./img/f3.jpg",
+    price: 40,
+    cat: "Sport",
+  },
+  {
+    id: 3,
+    name: "Breitling Superocean Heritage",
+    img: "./img/f4.jpg",
+    price: 200,
+    cat: "Luxury",
+  },
+  {
+    id: 4,
+    name: "Casio Classic Resin Strap ",
+    img: "./img/f5.jpg",
+    price: 16,
+    cat: "Sport",
+  },
+  {
+    id: 5,
+    name: "Garmin Venu Smartwatch ",
+    img: "./img/f6.jpg",
+    price: 74,
+    cat: "Casual",
+  },
+  {
+    id: 5,
+    name: "Garmin ",
+    img: "./img/f7.jpg",
+    price: 74,
+    cat: "Casual",
+  },
+  {
+    id: 5,
+    name: "Garmin Venu Random ",
+    img: "./img/f8.jpg",
+    price: 74,
+    cat: "Casual",
+  },
+];
+
